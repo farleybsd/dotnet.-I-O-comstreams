78bdbfbe0f7314e0cc1b43ed1bbc5022a8c20266
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-04-27 16:06:53 -0300

    Metodo Close e using

--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -16,18 +16,19 @@ namespace ByteBankImportacaoExportacao
 
             var enderecoDoArquivo = "contas.txt";
 
-            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open); // abrindo arquivo texto
-
-            var buffer = new byte[1024]; //numero de posiçoes
-
-            var numeroDeBytesLidos = -1;
-
-            while (numeroDeBytesLidos !=0) 
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))  // abrindo arquivo texto
             {
-                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024); // Parametro 1 numero de posices pametro 2 inicio da leitura parameto 3 final da leitura
-                EscreverBuffer(buffer);
-            }
+                var buffer = new byte[1024]; //numero de posiçoes
 
+                var numeroDeBytesLidos = -1;
+
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024); // Parametro 1 numero de posices pametro 2 inicio da leitura parameto 3 final da leitura
+                    EscreverBuffer(buffer);
+                }
+            }
+            
             Console.ReadLine();
         }
 
