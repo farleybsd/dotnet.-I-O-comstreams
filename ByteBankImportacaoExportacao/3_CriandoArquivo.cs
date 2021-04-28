5beb9921cd97ea88364f469b222093fbb82fc3a5
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-04-28 00:37:58 -0300

    Método Flush

--- ByteBankImportacaoExportacao/3_CriandoArquivo.cs
+++ ByteBankImportacaoExportacao/3_CriandoArquivo.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 
 namespace ByteBankImportacaoExportacao
@@ -29,6 +30,26 @@ namespace ByteBankImportacaoExportacao
                 using (var escritor = new StreamWriter(fluxoDoArquivo,Encoding.UTF8))
                 {
                     escritor.Write("456,78945,4785.50,Thalita Rabelo");
+                }
+            }
+        }
+
+        static void TestaEscrita()
+        {
+            var caminhoArquivo = "teste.txt";
+
+            using (var fluxoDoArquivo = new FileStream(caminhoArquivo,FileMode.Create))
+            {
+                using (var escritor = new StreamWriter(fluxoDoArquivo))
+                {
+                    for (int i = 0; i < 1000000; i++)
+                    {
+                        escritor.WriteLine($"Linha {i}");
+                        escritor.Flush(); // Despeja o buffer para o stream! ou seja escreve no arquivo em tempo real
+                        Console.WriteLine($"Linha {i}, foi escrita no arquivo.Aperte enter para adicionar uma nova");
+                        Console.ReadLine();
+                    }
+                    
                 }
             }
         }
