212afe23a5dea008dd7ea5bd2c13d83ba01ac4d7
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-04-28 09:20:15 -0300

    Usando o stream de entrada da Console

---
+++ ByteBankImportacaoExportacao/5_UsandoStreamDeEntradaDaConsole.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void UsarStreamDeEntrada()
+        {
+            using (var fluxoDeEntrada = Console.OpenStandardInput())
+            using (var fs = new FileStream("EntradaConsole.txt", FileMode.Create))
+            {
+                var buffer = new byte[1024]; // 1Kb
+                while (true)
+                {
+                    var bytesLidos = fluxoDeEntrada.Read(buffer, 0, 1024);
+
+                    fs.Write(buffer,0,bytesLidos);
+
+                    fs.Flush();
+
+                    Console.WriteLine($"Bytes Lidos da Console:{bytesLidos}");
+                }
+            }
+
+
+        }
+    }
+}
